b08ebe0a82a29d20579926d68102c6f8d849da55
itsblake10 <blake.cameron1001@gmail.com>
2023-12-16 21:13:27 +1300

    Project structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+      <ModalWithForm />
+      <ItemModal />
+    </div>
+  );
+}
+
+export default App;
